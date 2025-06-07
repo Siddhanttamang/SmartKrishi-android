f21436bc86a3e80472903607576813bdf13e5bac
rohitkganesh <rohitganesh1365@gmail.com>
2025-06-07 15:16:14 +0545

    modified design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+    implementation(libs.lottie)
+
 
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
