5d52a6c6a8f671bcf27b6df393d16dd993383f18
Siddhanttamang <tsiddhant2003@gmail.com>
2025-08-10 22:11:32 +0545

    deployed

--- app/src/main/java/com/example/smartkrishi/api/RetrofitClient.java
+++ app/src/main/java/com/example/smartkrishi/api/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.1.66:5000";
+    private static final String BASE_URL = "https://smartkrishi-backend.onrender.com/";
 
     private static Retrofit retrofit;
 
